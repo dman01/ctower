00bfbf61e894d061d88f0a1b480e196fdc9a2d96
Mark Randow <markrandow@Mark-Randows-iMac.local>
2011-11-20 17:54:40 +0100

    Truble with migration

--- db/migrate/20111119144042_create_portfolio_initiatives.rb
+++ db/migrate/20111119144042_create_portfolio_initiatives.rb
@@ -1,7 +1,8 @@
 class CreatePortfolioInitiatives < ActiveRecord::Migration
   def change
     create_table :portfolio_initiatives do |t|
-
+      t.integer :portfolioId
+      t.integer :initiativeId
       t.timestamps
     end
   end
