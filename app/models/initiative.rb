c4df989f7aae613d646cecfa71cf011f4fa8a0ea
Mark Randow <markrandow@Mark-Randows-iMac.local>
2011-11-20 16:54:41 +0100

    A bunch of new models

---
+++ app/models/initiative.rb
@@ -0,0 +1,5 @@
+class Initiative < ActiveRecord::Base
+  has_many :deliverables, :dependent =>destroy
+  has_and_belongs_to_many :portfolios
+
+end
---
+++ app/models/portfolio.rb
@@ -0,0 +1,3 @@
+class Portfolio < ActiveRecord::Base
+ has_and_belongs_to_many :initiatives
+end
