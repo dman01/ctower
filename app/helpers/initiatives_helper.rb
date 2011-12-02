cc6d426650d48df0eeee676158ee7511ce92c01f
Mark Randow <markrandow@Marks-MacBook-Air-2.local>
2011-12-02 15:21:50 +0100

    Pulled from Github

--- app/helpers/initiatives_helper.rb
+++ app/helpers/initiatives_helper.rb
@@ -1,20 +1,24 @@
 module InitiativesHelper
-  def list_portfolios(initiative_id)
-    #get the porfolios the user has access to
-     portfolios=Portfolio.list_by_user
 
-    #Create a multiple choice listbox
-    retval="<select name=\"initiative[portfolios]\" MULTIPLE SIZE=\"#{portfolios.count()}\">"
-    portfolios.each do |p|
-      retval+= "<option value=\"#{p.id}\"#{is_selected(initiative_id,p.id}>#{p.title}"
+  def list_portfolios(initiative_id)
+    #get the portfolios the user has access to
+    portfolios=Portfolio.list_by_user
+    i_exists=Initiative.exists?(initiative_id)
+    i=nil
+    if i_exists  then
+       i=Initiative.find(initiative_id)
     end
-    retval+="</select>"
+    retval=""
+    portfolios.each do |p|
+      sel=""
+      if i_exists
+        if i.portfolios.where("id=?",p.id).count()!=0 then
+          sel="selected"
+        end
+      end
+      retval+= "<option value=\"#{p.id}\"#{sel}>#{p.title}"
+    end
     retval.html_safe
   end
 
-  private
-
-  def is_selected
-
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,10 @@
 ActiveRecord::Schema.define(:version => 20111120191948) do
 
   create_table "Initiatives_Portfolios", :id => false, :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.integer  "portfolio_id"
     t.integer  "initiative_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "blogs", :force => true do |t|
