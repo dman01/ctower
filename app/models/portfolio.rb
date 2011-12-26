8e836d676e2264ab084780a89f7aca2050067950
Mark Randow <markrandow@Marks-MacBook-Air-2.local>
2011-12-26 12:27:31 +0100

    fiddled

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,7 @@
 class Portfolio < ActiveRecord::Base
  has_and_belongs_to_many :initiatives
  belongs_to :portfolio_process
+ validates :title, :description ,presence: true
 
   def self.list_by_user(user_id)
     Portfolio.all
